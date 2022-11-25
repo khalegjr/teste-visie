573169165619e1634f8844f76f0cad7f502b6406
Khaled <khalegjr@gmail.com>
2022-11-25 13:57:41 -0300

    cria conexão com base de dados

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ basedir = pathlib.Path(__file__).parent.resolve()
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
 app = connex_app.app
-app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql+mysqldb:{os.getenv('DB_USER')}:{os.getenv('DB_PASSWD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URI")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
